7391a4ba081e6ca3f5fe8fc1bbc00ac934e9b7c6
kou333 <naikkoustubh@gmail.com>
2024-11-16 16:23:45 +0530

    qr code scanner

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import QRCodeGenerator from './components/qr-code-generator';
+
+function App() {
+  return (
+    <div className="App">
+      <QRCodeGenerator/>
+    </div>
+  );
+}
+
+export default App;
